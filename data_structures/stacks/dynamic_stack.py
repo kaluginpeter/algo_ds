fa20549bbf90d85ad64b6776305e04c03df62fdb
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-10 15:14:04 +0300

    Added implementation of Dynamic Stack

--- data_structures/stacks/dynamic_stack.py
+++ data_structures/stacks/dynamic_stack.py
@@ -1,42 +1,39 @@
-from data_structures.arrays.static_multi_type_array import StaticMultiTypeArray
+from data_structures.arrays.dynamic_multi_type_array import DynamicMultiTypeArray
 
 
-class StaticStack:
+class DynamicStack:
     """
-    Implementation of Static Stack data structure.
-    Static stack using Static Multi Type Array for storing objects.
+    Implementation of Dynamic Stack data structure.
+    Static stack using Dynamic Multi TypeArray for storing objects.
     Static stack can store only one of the different data types, because
     implementation use some amortized operation and attempting to push
     some different types of object will throw an exception.
     Required params:
-    capacity: int - optional, by default sets to 10. Capacity will define size of stack.
     min_value_method: bool optional, by default sets to false. If true keep track minimum value in stack
     max_value_method: bool optional, by default sets to false. It true keep track maximum value in stack
     Example of usages:
-    ss = StaticStack(10)
+    ds = DynamicStack(10)
     For more information about available methods use all_methods() methods, like:
-    ss.all_methods()
+    ds.all_methods()
     """
-    def __init__(self, capacity: int = 10, min_value_method: bool = False, max_value_method: bool = False):
-        self.stack: StaticMultiTypeArray = StaticMultiTypeArray(capacity=capacity)
-        self.minimum_value_in_stack: StaticMultiTypeArray = StaticMultiTypeArray(capacity=capacity)
+    def __init__(self, min_value_method: bool = False, max_value_method: bool = False):
+        self.stack: DynamicMultiTypeArray = DynamicMultiTypeArray()
         self.min_value_method = min_value_method
         self.max_value_method = max_value_method
         if self.min_value_method:
-            self.minimum_value_in_stack: StaticMultiTypeArray = StaticMultiTypeArray(capacity=capacity)
+            self.minimum_value_in_stack: DynamicMultiTypeArray = DynamicMultiTypeArray()
         if self.max_value_method:
-            self.maximum_value_in_stack: StaticMultiTypeArray = StaticMultiTypeArray(capacity=capacity)
+            self.maximum_value_in_stack: DynamicMultiTypeArray = DynamicMultiTypeArray()
 
     def push(self, x: any) -> None:
         """
         Push element to the top of stack.
         Be careful in usage when attempting push object with different data type,
         stack use amortized min_value method, so pushing will throw exception during comparison elements.
-        Raise error if stack is full(have max size of defined capacity).
         Time complexity is O(1).
         Example of usages:
-        ss = StaticStack()
-        ss.push(1)
+        ds = DynamicStack()
+        ds.push(1)
         :param x:
         :return None:
         """
@@ -61,9 +58,9 @@ class StaticStack:
         Raise error if stack is empty.
         Time complexity is O(1).
         Example of usages:
-        ss = StaticStack()
-        ss.push(1)
-        ss.pop()
+        ds = DynamicStack()
+        ds.push(1)
+        ds.pop()
         :return object(popped element):
         """
         if self.min_value_method:
@@ -78,9 +75,9 @@ class StaticStack:
         Raise error if stack is empty.
         Time complexity is O(1)
         Example of usages:
-        ss = StaticStack()
-        ss.push(1)
-        ss.peek()
+        ds = DynamicStack()
+        ds.push(1)
+        ds.peek()
         :return object(element in stack):
         """
         if self.is_empty():
@@ -94,11 +91,13 @@ class StaticStack:
         method will throw exception during comparison between them.
         Time complexity amortized, so its O(1).
         Example of usages:
-        ss = StaticStack()
-        ss.push(1)
-        ss.min_value()
+        ds = DynamicStack()
+        ds.push(1)
+        ds.min_value()
         :return object(element in stack):
         """
+        if not self.min_value_method:
+            raise SystemError("Method not available! Make boolean min_value_method on True before usage")
         if self.is_empty():
             raise IndexError("Stack is empty")
         return self.minimum_value_in_stack[-1]
@@ -110,9 +109,9 @@ class StaticStack:
         method will throw exception during comparison between them.
         Time complexity amortized, so its O(1).
         Example of usages:
-        ss = StaticStack()
-        ss.push(1)
-        ss.max_value()
+        ds = DynamicStack()
+        ds.push(1)
+        ds.max_value()
         :return object(element in stack):
         """
         if not self.max_value_method:
@@ -126,8 +125,8 @@ class StaticStack:
         Return integer represent size(all elements store in stack) of stack.
         Time complexity amortized, so its O(1).
         Example of usages:
-        ss = StaticStack()
-        ss.length()
+        ds = DynamicStack()
+        ds.length()
         :return integer:
         """
         return self.stack.length()
@@ -136,28 +135,18 @@ class StaticStack:
         """
         Return True if stack is empty, otherwise return False.
         Example of usages:
-        ss = StaticStack()
-        ss.is_empty()
+        ds = DynamicStack()
+        ds.is_empty()
         :return boolean true of false:
         """
         return self.stack.is_empty()
-
-    def is_full(self) -> bool:
-        """
-        Return True if stack is full(have max elements in their size), otherwise return False.
-        Example of usages:
-        ss = StaticStack()
-        ss.is_full()
-        :return boolean true of false:
-        """
-        return self.stack.length() == self.stack.capacity
 
     def all_methods(self) -> list[str]:
         """
         Returning list names of all available methods.
         Example of usages:
-        ss = StaticStack()
-        ss.all_methods()
+        ds = DynamicStack()
+        ds.all_methods()
         :return list of strings:
         """
-        return dir(StaticStack)
+        return dir(DynamicStack)
