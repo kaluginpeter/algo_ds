3fcc27cbfa76d627de22e8f5396de8faca133a49
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-12 13:29:57 +0300

    Added implementation of string case checker is upper

---
+++ algorithms/string/is_upper.py
@@ -0,0 +1,17 @@
+def is_upper(string: str) -> bool:
+    """
+    Function that return if all alphabetical characters in string have upper case,
+    if it is, function return True, otherwise return False.
+    Time complexity O(N).
+    Memory Complexity O(1).
+    Example of Usages:
+    is_upper('S1') -> True
+    is_upper('hello') -> False
+    :param string: object of string instance.
+    :return: boolean True of False.
+    """
+    for character in string:
+        if character.isalpha():
+            if character != character.upper():
+                return False
+    return True
