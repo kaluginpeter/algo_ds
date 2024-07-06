747dfc70ce47999f77c2b20d844ebf92bcd54cee
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-06 11:45:56 +0300

    Added implementation of disjoint set union

---
+++ data_structures/set/disjoint_set_union.py
@@ -0,0 +1,133 @@
+class DisjointSetUnion:
+    """
+    Implementation of Disjoint Set Union data sturcture.
+    About Disjoint Set Union:
+    What is a Disjoint set data structure?
+    Two sets are called disjoint sets if they don’t have any element in common,
+    the intersection of sets is a null set.
+    A data structure that stores non overlapping or disjoint subset of elements is called disjoint set data structure.
+    The disjoint set data structure supports following operations:
+    Adding new sets to the disjoint set.
+    Merging disjoint sets to a single disjoint set using Union operation.
+    Finding representative of a disjoint set using Find operation.
+    Check if two sets are disjoint or not.
+    Consider a situation with a number of persons and the following tasks to be performed on them:
+    Add a new friendship relation, i.e. a person x becomes the friend of another person y i.e adding new element to a set.
+    Find whether individual x is a friend of individual y (direct or indirect friend)
+    """
+    def __init__(self, n: int) -> None:
+        """
+        Function that initialize disjoint set.
+        :param n: integer representing range of nodes, starting from 0 to n(not inclusive)
+        """
+        self.parents: list[int] = list(range(n))
+        self.ranks: list[int] = [1] * n
+        self.__connections: int = n
+
+    def union(self, x: int, y: int) -> bool:
+        """
+        Function that connect(union) given nodes together.
+        Return True if connections was successfully, otherwise return False,
+        if connections between nodes already exists.
+        :param x: Node of disjoint set
+        :param y: Node of disjoint set
+        :return: True if connection between node created and False if nodes already connected.
+        Time Complexity O(Inverse Ackermann function(N)), that almost equal to O(1).
+        Example of usages:
+        dsu = DisjointSetUnion(3)
+        dsu.union(0, 2) -> True
+        dsu.union(2, 0) -> False
+        """
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return False
+        if self.ranks[root_x] > self.ranks[root_y]:
+            self.parents[root_y] = root_x
+            self.ranks[root_x] += 1
+        elif self.ranks[root_x] < self.ranks[root_y]:
+            self.parents[root_x] = root_y
+            self.ranks[root_y] += 1
+        else:
+            self.parents[root_y] = root_x
+            self.ranks[root_x] += 1
+        self.__connections -= 1
+        return True
+
+    def find(self, x):
+        """
+        Function that find and return parent of given node.
+        Time Complexity O(Inverse Ackermann function(N)), that almost equal to O(1).
+        Example of Usages:
+        dsu = DisjointSetUnion(3)
+        dsu.find(2) -> 2
+        dsu.union(0, 2) -> True
+        dsu.find(2) -> 0
+        :param x: Node of disjoint set
+        :return: Parent of give node
+        """
+        root = x
+        while root != self.parents[root]:
+            root =  self.parents[root]
+        while x != root:
+            parent = self.parents[x]
+            self.parents[x] = root
+            x = parent
+        return root
+
+    def is_connected(self) -> bool:
+        """
+        Function that check if all nodes in disjoint set connected together.
+        If it is, function return True and False otherwise.
+        Time Complexity O(1).
+        Example of Usages:
+        dsu = DisjointSetUnion(3)
+        dsu.is_connected() -> False
+        for node in range(1, 3):
+            dsu.union(0, node) -> True
+        dsu.is_connected() -> True
+        :return: boolean True of False
+        """
+        return self.__connections == 1
+
+    def __str__(self) -> str:
+        """
+        Function that return object in his string representation. In next format:
+        "
+        Parents: node0: parentN, node1: parentN, ...
+        Ranks: node0: rank0, node1: rank1, ...
+        "
+        Time Complexity O(N).
+        Example of Usages:
+        dsu = DisjointSetUnion(3)
+        print(dsu) -> Parents: {0: 0, 1: 1, 2: 2}
+                      Ranks: {0: 1, 1: 1, 2: 1}
+        :return: string representation of object
+        """
+        parents = ', '.join(f'{node}: {parent}' for node, parent in enumerate(self.parents))
+        ranks = ', '.join(f'{node}: {rank}' for node, rank in enumerate(self.ranks))
+        return f'Parents: {{{parents}}}\nRanks: {{{ranks}}}'
+
+    def __repr__(self) -> str:
+        """
+        Function that return string representation of class/object. In next format:
+        "
+        Parents: node0: parentN, node1: parentN, ...\nRanks: node0: rank0, node1: rank1, ...
+        "
+        Time Complexity O(N).
+        Example of Usages:
+        dsu = DisjointSetUnion(3)
+        repr(dsu) -> Parents: {0: 0, 1: 1, 2: 2}\nRanks: {0: 1, 1: 1, 2: 1}
+        :return: string representation of class/object.
+        """
+        return self.__str__()
+
+    def all_methods(self) -> list[str]:
+        """
+        Function that return list of names available methods in class.
+        Example of Usages:
+        dsu = DisjointSetUnion(10)
+        dsu.all_methods() -> list[str, str, ...]
+        :return: list contains string representing names of available methods in class.
+        """
+        return dir(DisjointSetUnion)
